712935cbd670aa7e17aeb22f4a3e4f75540c9cdd
Ivan8R <cotedeazur@gmail.com>
2024-03-25 13:50:32 +0300

    transformation work properly and file save without canvas

--- src/components/modalView.vue
+++ src/components/modalView.vue
@@ -15,7 +15,7 @@
                 <button
                   type="button"
                   @click="close"
-                  class="float-right text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-fit inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  class="float-right p-2 border text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-fit inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                   data-modal-hide="default-modal"
                 >
                   <svg
@@ -54,7 +54,7 @@
                     class="w-fit mx-auto"
                   >
                     <path
-                      d="M386.3 160H336c-17.7 0-32 14.3-32 32s14.3 32 32 32H464c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32s-32 14.3-32 32v51.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0s-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3s163.8-62.5 226.3 0L386.3 160z"
+                      d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5c0 0 0 0 0 0H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v7.6 .7V440c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2s-12.5-14.8-22.2-14.8H48.4h-.7H40c-13.3 0-24 10.7-24 24z"
                     />
                   </svg>
                   Orientation
@@ -273,8 +273,28 @@
           </div>
           <!-- Modal footer -->
           <div
-            class="flex justify-end items-center p-2 md:p-2 border-gray-200 rounded-b dark:border-gray-600"
+            class="flex justify-end items-center border-gray-200 rounded-b dark:border-gray-600 mt-20"
           >
+            <div class="flex-1 w-fit pl-2">
+              <button
+                class="p-2 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-fit inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-3 h-3">
+                  <path
+                    d="M205 34.8c11.5 5.1 19 16.6 19 29.2v64H336c97.2 0 176 78.8 176 176c0 113.3-81.5 163.9-100.2 174.1c-2.5 1.4-5.3 1.9-8.1 1.9c-10.9 0-19.7-8.9-19.7-19.7c0-7.5 4.3-14.4 9.8-19.5c9.4-8.8 22.2-26.4 22.2-56.7c0-53-43-96-96-96H224v64c0 12.6-7.4 24.1-19 29.2s-25 3-34.4-5.4l-160-144C3.9 225.7 0 217.1 0 208s3.9-17.7 10.6-23.8l160-144c9.4-8.5 22.9-10.6 34.4-5.4z"
+                  />
+                </svg>
+              </button>
+              <button
+                class="ml-1 p-2 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-fit inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-3 h-3">
+                  <path
+                    d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z"
+                  />
+                </svg>
+              </button>
+            </div>
             <div v-if="operator">
               <div v-if="operator === 'Adjust'">
                 <button
@@ -346,6 +366,10 @@
   const contrast = ref(0)
   const saturation = ref(0)
   const angelRotation = ref(0)
+  const minX = 0
+  const maxX = 600
+  const minY = 0
+  const maxY = 400
   const canvasReference = ref()
   let canvas: any = null
   let filters: any = null
@@ -361,83 +385,146 @@
   )
 
   onMounted(() => {
-    if (props.image) {
-      filters = {
-        brightness: new fabric.Image.filters.Brightness(),
-        saturation: new fabric.Image.filters.Saturation(),
-        contrast: new fabric.Image.filters.Contrast()
+    canvas = new fabric.Canvas(canvasReference.value, {
+      containerClass: 'w-20 block mx-auto flex',
+      isDrawingMode: false,
+      selection: false
+    })
+
+    canvas.on('object:moving', function (e: any) {
+      const obj = e.target
+      obj.setCoords()
+      const b = obj.getBoundingRect()
+      if (!(b.left >= minX && maxX >= b.left + b.width)) {
+        obj.left = obj.lastLeft
+        obj.scaleX = obj.lastScaleX
+        obj.scaleY = obj.lastScaleY
+      } else {
+        obj.lastLeft = obj.left
+        obj.lastScaleX = obj.scaleX
       }
-      canvas = new fabric.Canvas(canvasReference.value, {
-        containerClass: 'w-fit h-fit block mx-auto flex',
-        isDrawingMode: false
-      })
-      canvas.setWidth(400)
-      canvas.setHeight(400)
-      var minX = 0
-      var maxX = 400
-      var minY = 0
-      var maxY = 400
-
-      canvas.on('object:moving', function (e: any) {
-        var obj = e.target
-        obj.setCoords()
-        var b = obj.getBoundingRect()
-        if (!(b.left >= minX && maxX >= b.left + b.width)) {
-          obj.left = obj.lastLeft
-          obj.scaleX = obj.lastScaleX
-          obj.scaleY = obj.lastScaleY
-        } else {
-          obj.lastLeft = obj.left
-          obj.lastScaleX = obj.scaleX
-        }
-        if (!(maxY >= b.top + b.height && b.top >= minY)) {
-          obj.top = obj.lastTop
-          obj.scaleX = obj.lastScaleX
-          obj.scaleY = obj.lastScaleY
-        } else {
-          obj.lastTop = obj.top
-          obj.lastScaleY = obj.scaleY
-        }
-      })
-
-      fabric.Image.fromURL(props.image, (_img: any) => {
-        _img._element.crossOrigin = 'anonymous'
-        _img.set({
-          top: 0,
-          left: 0,
-          selectable: false,
-          hoverCursor: 'default'
-        })
-        _img.filters.push(filters.brightness)
-        _img.filters.push(filters.saturation)
-        _img.filters.push(filters.contrast)
-
-        _img.scaleToWidth(400, false)
-        canvas.centerObject(_img)
-        canvas.add(_img)
-      })
-    }
-    canvas.renderAll()
+      if (!(maxY >= b.top + b.height && b.top >= minY)) {
+        obj.top = obj.lastTop
+        obj.scaleX = obj.lastScaleX
+        obj.scaleY = obj.lastScaleY
+      } else {
+        obj.lastTop = obj.top
+        obj.lastScaleY = obj.scaleY
+      }
+    })
+    initImage(props.image, canvas)
   })
+
   const props = defineProps<{
     modalActive: boolean
     image: string
   }>()
+
   const emit = defineEmits(['close'])
   const close = () => {
     operator.value = ''
     emit('close')
   }
-  const save = () => {
-    let w = cropZone.aCoords.tr.x - cropZone.aCoords.tl.x
-    let h = cropZone.aCoords.bl.y - cropZone.aCoords.tl.y
+
+  function initImage(src: any, canvas: any) {
+    filters = {
+      brightness: new fabric.Image.filters.Brightness(),
+      saturation: new fabric.Image.filters.Saturation(),
+      contrast: new fabric.Image.filters.Contrast()
+    }
+    let imgObj = new Image()
+
+    imgObj.crossOrigin = 'anonymous'
+    imgObj.src = src
+    imgObj.onload = function () {
+      let _img = new fabric.Image(imgObj)
+      _img.set({
+        top: 0,
+        left: 0,
+        selectable: false,
+        hoverCursor: 'default'
+      })
+      _img.filters.push(filters.brightness)
+      _img.filters.push(filters.saturation)
+      _img.filters.push(filters.contrast)
+
+      let viewport = computeImageViewPort(_img)
+      let vieportWithScale = calculateAspectRatioFit(viewport.width, viewport.height, maxX, maxY)
+      _img.scaleToHeight(vieportWithScale.height)
+      _img.scaleToWidth(vieportWithScale.width)
+      canvas.setHeight(vieportWithScale.height)
+      canvas.setWidth(vieportWithScale.width)
+      canvas.add(_img)
+    }
+  }
+
+  /**
+   * Conserve aspect ratio of the original region. Useful when shrinking/enlarging
+   * images to fit into a certain area.
+   *
+   * @param {Number} srcWidth width of source image
+   * @param {Number} srcHeight height of source image
+   * @param {Number} maxWidth maximum available width
+   * @param {Number} maxHeight maximum available height
+   * @return {Object} { width, height }
+   */
+  function calculateAspectRatioFit(srcWidth: any, srcHeight: any, maxWidth: any, maxHeight: any) {
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight)
+
+    return { width: srcWidth * ratio, height: srcHeight * ratio }
+  }
+
+  /**
+   * Calculate width height for canvas according to Image
+   *
+   *
+   * @param {Image} Image object
+   * @return {Object} { width, height }
+   */
+
+  function computeImageViewPort(image: any) {
+    let height =
+      Math.abs(image.width * Math.sin((image.angle * Math.PI) / 180)) +
+      Math.abs(image.height * Math.cos((image.angle * Math.PI) / 180))
+    let width =
+      Math.abs(image.width * Math.sin((image.angle * Math.PI) / 180)) +
+      Math.abs(image.width * Math.cos((image.angle * Math.PI) / 180))
+    return {
+      height,
+      width
+    }
+  }
+  function applyTransformation(canvas: any, image: any, newAgle: any) {
+    let angle = (image.angle + newAgle) % 360
+    image.rotate(angle)
+
+    let width, height
+    height =
+      Math.abs(image.width * Math.sin((angle * Math.PI) / 180)) +
+      Math.abs(image.height * Math.cos((angle * Math.PI) / 180))
+    width =
+      Math.abs(image.height * Math.sin((angle * Math.PI) / 180)) +
+      Math.abs(image.width * Math.cos((angle * Math.PI) / 180))
+
+    let vieportWithScale = calculateAspectRatioFit(width, height, maxX, maxY)
+
+    canvas.setWidth(vieportWithScale.width)
+    canvas.setHeight(vieportWithScale.height)
+    image.scaleToWidth(vieportWithScale.width)
+    image.scaleToHeight(vieportWithScale.height)
+
+    canvas.centerObject(image)
+    image.setCoords()
+    canvas.renderAll()
+  }
+
+  function save() {
     const base64 = canvas.toDataURL({
       format: 'webp',
       enableRetinaScaling: true,
-      left: cropZone.left,
-      top: cropZone.top,
-      height: h,
-      width: w
+      left: 0,
+      top: 0,
+      quality: 1
     })
     var a = document.createElement('a')
     a.href = base64
@@ -449,7 +536,6 @@
     filters.brightness.brightness = brightness.value * 0.001
     canvas.getObjects()[0].applyFilters()
     canvas.renderAll()
-    console.log(canvas.toJSON())
   }
   function contrastApply() {
     filters.contrast.contrast = contrast.value * 0.001
@@ -457,7 +543,7 @@
     canvas.renderAll()
   }
   function saturationApply() {
-    filters.saturation.saturation = saturation.value * 0.001
+    filters.saturation.saturation = saturation.value * 0.01
     canvas.getObjects()[0].applyFilters()
     canvas.renderAll()
   }
@@ -476,35 +562,19 @@
 
   function rotateMinus() {
     angelRotation.value--
-    canvas.getObjects()[0]._setOriginToCenter()
-    canvas.getObjects()[0].set('angle', angelRotation.value)
-    canvas.renderAll()
+    applyTransformation(canvas, canvas.getObjects()[0], -1)
   }
   function rotatePlus() {
     angelRotation.value++
-    canvas.getObjects()[0]._setOriginToCenter()
-    canvas.getObjects()[0].set('angle', angelRotation.value)
-    canvas.renderAll()
+    applyTransformation(canvas, canvas.getObjects()[0], 1)
   }
 
   function rotatePlus90() {
-    let obj = canvas.getObjects()[0]
-    let angle = obj.get('angle')
-    let newAngle = angle + 90
-    canvas.getObjects()[0]._setOriginToCenter()
-    canvas.getObjects()[0].set('angle', newAngle)
-    canvas.renderAll()
-    angelRotation.value = angelRotation.value + 90
+    applyTransformation(canvas, canvas.getObjects()[0], 90)
   }
 
   function rotateMinus90() {
-    let obj = canvas.getObjects()[0]
-    let angle = obj.get('angle')
-    let newAngle = angle - 90
-    canvas.getObjects()[0]._setOriginToCenter()
-    canvas.getObjects()[0].set('angle', newAngle)
-    canvas.renderAll()
-    angelRotation.value = angelRotation.value - 90
+    applyTransformation(canvas, canvas.getObjects()[0], -90)
   }
   function flipHorizontally() {
     let obj = canvas.getObjects()[0]
@@ -518,34 +588,50 @@
   }
 
   function addCrop() {
-    let obj = canvas.getObjects()[0]
-    cropZone = new fabric.Rect({
-      width: canvas.getObjects()[0].width / 8,
-      height: canvas.getObjects()[0].height / 8,
-      left: (obj.aCoords.tr.x - obj.aCoords.tl.x - 200) / 2,
-      top: (obj.aCoords.bl.y - obj.aCoords.tl.y) / 2,
-      opacity: 0.2,
-      hasRotatingPoint: false,
-      transparentCorners: false,
-      cornerColor: 'white',
-      cornerStrokeColor: 'black',
-      borderColor: 'black',
-      stroke: 'black',
-      cornerStyle: 'circle',
-      borderDashArray: [5, 5],
-      absolutePositioned: true
-    })
-    canvas.add(cropZone)
-    canvas.setActiveObject(canvas.item(1))
-  }
-  function resetCrop() {
-    canvas.remove(cropZone)
-    canvas.discardActiveObject().renderAll()
+    if (canvas.getObjects().length == 1) {
+      cropZone = new fabric.Rect({
+        width: canvas.width / 3,
+        height: canvas.height / 3,
+        left: (canvas.width - canvas.width / 3) / 2,
+        top: (canvas.height - canvas.height / 3) / 2,
+        opacity: 0.2,
+        lockRotation: true,
+        transparentCorners: false,
+        cornerColor: 'white',
+        cornerStrokeColor: 'black',
+        borderColor: 'white',
+        stroke: 'black',
+        cornerStyle: 'circle',
+        borderDashArray: [5, 5],
+        absolutePositioned: true
+      })
+      canvas.add(cropZone)
+    }
+    canvas.setActiveObject(canvas.getObjects()[1])
   }
 
   function cropArea() {
     canvas.getObjects()[0].clipPath = cropZone
+    canvas.discardActiveObject().renderAll()
+    let w = cropZone.aCoords.tr.x - cropZone.aCoords.tl.x
+    let h = cropZone.aCoords.bl.y - cropZone.aCoords.tl.y
+    const base64 = canvas.toDataURL({
+      format: 'webp',
+      enableRetinaScaling: true,
+      left: cropZone.left,
+      top: cropZone.top,
+      height: h,
+      width: w,
+      quality: 1
+    })
+    initImage(base64, canvas)
+    canvas.remove(canvas.getObjects()[0])
     canvas.remove(cropZone)
+    canvas.discardActiveObject().renderAll()
+  }
+  function resetCrop() {
+    canvas.remove(cropZone)
+    cropZone = null
     canvas.discardActiveObject().renderAll()
   }
 </script>
